b357c50e7c0f76e405e3ade6ab653a1a2ce1883b
Mary Green <cyberangel9911@gmail.com>
2019-08-04 16:29:11 -0500

    cart
    added cart product data for the cart array

--- cart.js
+++ cart.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
+    //If the cart does not exist
+    if(!localStorage.getItem('cart')) {//Initialize the cart if it doesn't exist
+        localStorage.setItem('cart', JSON.stringify([]))//used stringify to turn the array to a string for local storage, local storage wants everything into a string
+    }
     // Load products file
     $("#products-hidden").load('products.json', function (results) {
-        const products = JSON.parse(results) // Our products, .parse converts the string into json
+        const products = JSON.parse(results) // Our products, .parse converts the results string into json
         localStorage.setItem('products', results)
         products.map(function (product) {// .map loops the products, jquery prepend inserts content at the beginning of the elements
             showProducts(product)
@@ -17,14 +21,27 @@ $(document).ready(function () {
         $("#products").html("");
         //shows the product that is searched
         products.filter(p => {
+            console.log(p.name.toLowerCase().indexOf(term))
             if (p.name.toLowerCase().indexOf(term) > -1) {
                 showProducts(p);
             }
         })
     })
+
+    $(document).on("click", ".cart-button", function() {//this event is done after the page is loaded and the products are loaded, use only document .on("click")
+       let product = $(this).parent('.card-body').find('.prod').val()
+       product = parseInt(product) //parseInt turns the id from a string into a integer or number from the json file.
+       addToCart(product);
+    });
 });
 
+//this function is outside of document ready so it won't start until it is called.
 // Refactored (abstracted) code to keep the codebase DRY (Don't Repeat Yourself)
+ addToCart = product => {
+    let cart = JSON.parse(localStorage.getItem('cart'))//takes the string array from local storage and turns it into an array 
+    cart.push(product);//pushes the product to the cart array
+    localStorage.setItem('cart', JSON.stringify(cart))//reassigning the new cart to the cart key in local storage. reseting the cart so it shows the product in the new cart array
+ }
  showProducts = product => {
     $("#products").prepend(`
         <div class="col-md-4 mt-4">
@@ -34,8 +51,11 @@ $(document).ready(function () {
                     <h5 class="card-title">${product.name}</h5>
                     <p class="card-text">${product.description}</p>
                     <p class="card-text">${product.price}</p>
+                    <button type="button" class="btn btn-primary cart-button">Add to Cart</button>
+                    <input class="prod" type="hidden" value=${product.id} />
                 </div>
             </div>
         </div>
     `)
 }
+
